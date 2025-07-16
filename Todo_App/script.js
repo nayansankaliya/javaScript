086c7fe8efdf038ae17999767ba4e050fcc4efca
Vaibhav <vaibhav.chavda@digiqt.com>
2025-07-16 15:10:58 +0530

    todo app

---
+++ Todo_App/script.js
@@ -0,0 +1,38 @@
+let todoList = [
+    // {
+    //   item: 'Exercise',
+    //   dueDate: '2025-07-16'
+    // },
+    // {
+    //   item: 'Go to office',
+    //   dueDate: '2025-07-16'
+    // }
+  ];
+  
+  displayItems();
+  
+  function addTodo() {
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
+    inputElement.value = '';
+    dateElement.value = '';
+    displayItems();
+  }
+  
+  function displayItems() {
+    let containerElement = document.querySelector('.todo-container');
+    let newHtml = '';
+    for (let i = 0; i < todoList.length; i++) {
+      let {item, dueDate} = todoList[i];
+      newHtml += `
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+        displayItems();">Delete</button>
+      `;
+    }
+    containerElement.innerHTML = newHtml;
+  }
