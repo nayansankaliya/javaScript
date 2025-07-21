61f9cf74f300b4057671bd4f9255701d5de22b38
Vaibhav <vaibhav.chavda@digiqt.com>
2025-07-21 17:27:03 +0530

    to-do updated

--- Todo_App/script.js
+++ Todo_App/script.js
@@ -1,38 +1,40 @@
-let todoList = [
-    // {
-    //   item: 'Exercise',
-    //   dueDate: '2025-07-16'
-    // },
-    // {
-    //   item: 'Go to office',
-    //   dueDate: '2025-07-16'
-    // }
-  ];
-  
+let todoList = [];
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value.trim();
+  let todoDate = dateElement.value;
+
+  if (todoItem === '' || todoDate === '') {
+    alert("Field cannot be empty!");
+    return;
+  }
+
+  todoList.push({ item: todoItem, dueDate: todoDate });
+  inputElement.value = '';
+  dateElement.value = '';
   displayItems();
-  
-  function addTodo() {
-    let inputElement = document.querySelector('#todo-input');
-    let dateElement = document.querySelector('#todo-date');
-    let todoItem = inputElement.value;
-    let todoDate = dateElement.value;
-    todoList.push({item: todoItem, dueDate: todoDate});
-    inputElement.value = '';
-    dateElement.value = '';
-    displayItems();
-  }
-  
-  function displayItems() {
-    let containerElement = document.querySelector('.todo-container');
-    let newHtml = '';
-    for (let i = 0; i < todoList.length; i++) {
-      let {item, dueDate} = todoList[i];
-      newHtml += `
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
-        displayItems();">Delete</button>
-      `;
-    }
-    containerElement.innerHTML = newHtml;
-  }
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  containerElement.innerHTML = '';
+
+  todoList.forEach((todo, index) => {
+    const todoDiv = document.createElement('div');
+    todoDiv.className = 'todo-item';
+
+    todoDiv.innerHTML = `
+      <span>${todo.item}</span>
+      <span>${todo.dueDate}</span>
+      <button class="btn-delete" onclick="deleteTodo(${index})">Delete</button>
+    `;
+    containerElement.appendChild(todoDiv);
+  });
+}
+
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  displayItems();
+}
